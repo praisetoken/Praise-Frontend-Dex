0f6957f1774595a4760a8c81aa53196dfef6da2d
praisetoken <99622506+praisetoken@users.noreply.github.com>
2022-02-25 23:06:40 -0500

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -18,87 +18,18 @@ const config: MenuEntry[] = [
       {
         label: 'Liquidity',
         href: '/pool',
-      },
+       },
     ],
-  
-  // {
-  //   label: 'Lottery',
-  //   icon: 'TicketIcon',
-  //   href: 'https://siacashcoin.com/lottery',
-  // },
+  },
+
+
   // {
   //   label: 'NFT',
   //   icon: 'NftIcon',
-  //   href: 'https://siacashcoin.com/nft',
+  //   href: '/',
   // },
   // {
-  //   label: 'Teams & Profile',
-  //   icon: 'GroupsIcon',
-  //   calloutClass: 'rainbow',
-  //   items: [
-  //     {
-  //       label: 'Leaderboard',
-  //       href: 'https://siacashcoin.com/teams',
-  //     },
-  //     {
-  //       label: 'Task Center',
-  //       href: 'https://siacashcoin.com/profile/tasks',
-  //     },
-  //     {
-  //       label: 'Your Profile',
-  //       href: 'https://siacashcoin.com/profile',
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: 'Info',
-  //   icon: 'InfoIcon',
-  //   items: [
-  //     {
-  //       label: 'Overview',
-  //       href: 'https://pancakeswap.info',
-  //     },
-  //     {
-  //       label: 'Tokens',
-  //       href: 'https://pancakeswap.info/tokens',
-  //     },
-  //     {
-  //       label: 'Pairs',
-  //       href: 'https://pancakeswap.info/pairs',
-  //     },
-  //     {
-  //       label: 'Accounts',
-  //       href: 'https://pancakeswap.info/accounts',
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: 'IFO',
-  //   icon: 'IfoIcon',
-  //   href: 'https://siacashcoin.com/ifo',
-  // },
-  // {
-  //   label: 'More',
-  //   icon: 'MoreIcon',
-  //   items: [
-  //     {
-  //       label: 'Voting',
-  //       href: 'https://voting.siacashcoin.com',
-  //     },
-  //     {
-  //       label: 'Github',
-  //       href: 'https://github.com/pancakeswap',
-  //     },
-  //     {
-  //       label: 'Docs',
-  //       href: 'https://docs.siacashcoin.com',
-  //     },
-  //     {
-  //       label: 'Blog',
-  //       href: 'https://pancakeswap.medium.com',
-  //     },
-  //   ],
-  // },
+
 ]
 
 export default config
